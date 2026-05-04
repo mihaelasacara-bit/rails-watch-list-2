e20e02576e0af30f8327c82aff4dbf12380c7379
Mihaela Sacara <mihaelasacara@gmail.com>
2026-05-04 18:24:18 +0200

    seeded movies with themoviedb api call

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "json"
+require "open-uri"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+
+data = JSON.parse(URI.open(url).read)
+
+data["results"].each do |m|
+  Movie.create!(
+    title:      m["title"],
+    overview:   m["overview"],
+    rating:     m["vote_average"],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{m["poster_path"]}.jpg"
+  )
+end
